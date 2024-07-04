83148c1b3003ba7b4ae23860b2a480a0b26dd8be
Salma158 <salmasherif856@gmail.com>
2024-07-04 11:02:49 +0300

    added spring securityFilterChain config

--- src/main/java/com/example/auth2/config/SecurityConfig.java
+++ src/main/java/com/example/auth2/config/SecurityConfig.java
@@ -1,21 +1,24 @@
 package com.example.auth2.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebFluxSecurity
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHTTPSecurity){
-        serverHTTPSecurity.authorizeExchange( exchangs -> exchangs.pathMatchers(HttpMethod.GET).permitAll()
-                .pathMatchers("/items").authenticated()).oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
-        serverHTTPSecurity.csrf(csrfSpec -> csrfSpec.disable());
-        return serverHTTPSecurity.build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
+        return http.build();
     }
 }
